9d10c4e86114807e2ec6de403dbcba9875e53ba1
irina_gurina <irinagurina0612@gmail.com>
2025-10-22 12:11:42 +0300

    small changes

---
+++ AllPatterns/BehavioralPatterns/Iterator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralPatterns
+{
+    public interface ProfileIterator
+    {
+        Profile getNext();
+        bool hasNext();
+    }
+    public class FacebookIterator : ProfileIterator
+    {
+        public Facebook Facebook {  get; set; }
+        public string profileId {  get; set; }
+
+        public FacebookIterator() { }
+    }
+
+    public interface SocialNetwork
+    {
+        ProfileIterator createFriendsIterator(int profileId);
+        ProfileIterator createCoworkersIterator(int profileId);
+    }
+
+    public class Facebook : SocialNetwork
+    {
+
+    }
+
+    public class Profile { }
+}
---
+++ AllPatterns/BehavioralPatterns/Mediator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralPatterns
+{
+    public interface IMediator
+    {
+        void Notify(Component initiator, string message);
+    }
+    public class Mediator : IMediator
+    {
+        public ComponentTextBox textBox {  get; set; }
+        public ComponentButton button { get; set; }
+
+        public void Notify(Component initiator, string message)
+        {
+            if (initiator is ComponentTextBox)
+            {
+                Console.WriteLine("Business logic text box changed");
+            }
+            else if (initiator is ComponentButton)
+            {
+                Console.WriteLine("Business logic button pushed");
+            }
+        }
+    }
+
+    public abstract class Component 
+    {
+        public IMediator Mediator;
+        public Component(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+
+    public class ComponentTextBox : Component
+    {
+        public ComponentTextBox(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public void ChangeText()
+        {
+                Mediator.Notify(this, "Changed text in TextBox");
+        }
+    }
+    public class ComponentButton : Component
+    {
+        public ComponentButton(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public void Push()
+        {
+            Mediator.Notify(this, "Pushed the button");
+        }
+    }
+
+}
